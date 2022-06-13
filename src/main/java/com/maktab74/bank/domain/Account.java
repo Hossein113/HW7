46b95514f4d7c4d77accf925c70559825d9ba837
Hossein113 <hos_m18@yahoo.com>
2022-06-13 11:29:48 -0700

    update Account class

--- src/main/java/com/maktab74/bank/domain/Account.java
+++ src/main/java/com/maktab74/bank/domain/Account.java
@@ -1,4 +1,45 @@
 package com.maktab74.bank.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    @ManyToOne
+    private Customer customer;
+
+    @OneToOne
+    private Cart cart;
+    @Column(unique = true)
+    private Long AcountCodeNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getAcountCodeNumber() {
+        return AcountCodeNumber;
+    }
+
+    public void setAcountCodeNumber(Long acountCodeNumber) {
+        AcountCodeNumber = acountCodeNumber;
+    }
 }
