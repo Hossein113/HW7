af7342ceb93f6ea26077a59782908e590c2f1cf1
Hossein113 <hos_m18@yahoo.com>
2022-06-13 11:30:09 -0700

    update Application class

--- src/main/java/com/maktab74/bank/Application.java
+++ src/main/java/com/maktab74/bank/Application.java
@@ -1,4 +1,18 @@
 package com.maktab74.bank;
 
+import com.maktab74.bank.util.HibernateUtil;
+
 public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("start");
+
+
+        HibernateUtil.getEntitymanagerfactory().createEntityManager();
+
+
+        System.out.println("END");
+
+    }
 }
