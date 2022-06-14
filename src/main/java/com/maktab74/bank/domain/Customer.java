1fa6a25345c6dbe09f19ab94ea359836a822a699
Hossein113 <hos_m18@yahoo.com>
2022-06-13 21:18:39 -0700

    Update customer  Class

--- src/main/java/com/maktab74/bank/domain/Customer.java
+++ src/main/java/com/maktab74/bank/domain/Customer.java
@@ -2,52 +2,37 @@ package com.maktab74.bank.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 @Entity
 public class Customer extends User {
 
 
-    @Id
-    @GeneratedValue
-    private Long id;
-
     @Column(unique = true)
-    private Long Customer;
+    private Long customerCodeNumber;
 
 
     public Customer() {
+
     }
 
-
-    public Customer(Long id, Long customer) {
-        this.id = id;
-        Customer = customer;
+    public Customer(String firstName, String lastName, String userName, String gender,
+                    Long age, String phoneNumber, String password, Long customerCodeNumber) {
+        super(firstName, lastName, userName, gender, age, phoneNumber, password);
+        this.customerCodeNumber = customerCodeNumber;
     }
 
-    public Customer(Long id, String firstName, String lastName, String userName, String gender,
-                    Long age, String phoneNumber, String password, Long id1, Long customer) {
-        super(id, firstName, lastName, userName, gender, age, phoneNumber, password);
-        this.id = id1;
-        Customer = customer;
+    public Long getCustomerCodeNumber() {
+        return customerCodeNumber;
     }
 
-    @Override
-    public Long getId() {
-        return id;
+    public void setCustomerCodeNumber(Long customerCodeNumber) {
+        this.customerCodeNumber = customerCodeNumber;
     }
 
     @Override
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getCustomer() {
-        return Customer;
-    }
-
-    public void setCustomer(Long customer) {
-        Customer = customer;
+    public String toString() {
+        return "Customer{" +
+                "customerCodeNumber=" + customerCodeNumber +
+                "} " + super.toString();
     }
 }
