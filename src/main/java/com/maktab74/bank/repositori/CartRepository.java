ac8afb1fd013fffdddbcf7ea5a84b32a6a925239
Hossein113 <hos_m18@yahoo.com>
2022-06-14 18:57:51 -0700

    update CartRepository class

--- src/main/java/com/maktab74/bank/repositori/CartRepository.java
+++ src/main/java/com/maktab74/bank/repositori/CartRepository.java
@@ -1,17 +1,25 @@
 package com.maktab74.bank.repositori;
 
 import com.maktab74.bank.base.reposity.BaseRepository;
+import com.maktab74.bank.domain.Account;
 import com.maktab74.bank.domain.Cart;
 import com.maktab74.bank.util.CartBrief;
+
+import java.util.List;
 
 public interface CartRepository extends BaseRepository<Cart, Long> {
 
     Cart findByAccuntId(Long id);
 
+    Cart findByAccuntId(Account account);
 
     Cart chekCart(CartBrief cartBrief);
 
     Cart destination(String cart);
+
+    void deletByAcount(Account account);
+
+    List<Cart> findAllById(Long id);
 
 
 }
