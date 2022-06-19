6396448b13c55bef5f6dabcd1cdfdab8c5349e4b
Hossein113 <hos_m18@yahoo.com>
2022-06-18 18:16:55 -0700

    update TransactionRepository

--- src/main/java/com/maktab74/bank/repositori/TransactionRepository.java
+++ src/main/java/com/maktab74/bank/repositori/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.maktab74.bank.repositori;
 
 import com.maktab74.bank.base.reposity.BaseRepository;
+import com.maktab74.bank.domain.Cart;
 import com.maktab74.bank.domain.Transaction;
 
 import java.util.List;
@@ -9,4 +10,6 @@ public interface TransactionRepository extends BaseRepository<Transaction, Long>
 
 
     List<Transaction> listTransaction(Long id);
+
+    List<Transaction> searchTransaction(Cart cart);
 }
