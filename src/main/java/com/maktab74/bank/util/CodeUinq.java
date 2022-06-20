48cb1db15afc30266d5322c2a458ab8454df3a1c
Hossein113 <hos_m18@yahoo.com>
2022-06-20 12:36:37 -0700

    update CodeUinq

--- src/main/java/com/maktab74/bank/util/CodeUinq.java
+++ src/main/java/com/maktab74/bank/util/CodeUinq.java
@@ -4,26 +4,75 @@ import java.util.Random;
 
 public class CodeUinq {
     public static void main(String[] args) {
-        int a = randomCcv2();
 
+        int[] randomArrayCardNumber = RandomCardNumber();
+
+        System.out.println(arratToIntegerCode(randomArrayCardNumber));
+    }
+
+
+    public static int[] RandomCardNumber() {
+
+        int[] arraysNumber = new int[16];
+
+        for (int i = 0; i < arraysNumber.length; i++) {
+
+            arraysNumber[i] = ApplicationContext.random.nextInt(10);
+
+        }
+        return arraysNumber;
+    }
+
+    public static int[] RandomAccountNumber() {
+
+        int[] arraysNumber = new int[18];
+
+        for (int i = 0; i < arraysNumber.length; i++) {
+
+            arraysNumber[i] = ApplicationContext.random.nextInt(10);
+
+        }
+        return arraysNumber;
+    }
+
+    public static Long arratToIntegerCode(int[] number) {
+
+        long code = 0;
+
+        for (int i = 0; i < number.length; i++) {
+
+            code = (code * 10) + number[i];
+        }
+        return code;
     }
 
     public static long randomCode() {
         Random random = new Random();
         int randomCode = random.nextInt(999999999);
+
         return randomCode;
     }
 
-    public static int randomCcv2() {
+    public static int[] randomCcv2() {
 
-        Random random = new Random();
-        int randomCvv2 = random.nextInt(8999) + 1000;
-        return randomCvv2;
+        int[] arraysNumber = new int[4];
+
+        for (int i = 0; i < arraysNumber.length; i++) {
+
+            arraysNumber[i] = ApplicationContext.random.nextInt(10);
+        }
+        return arraysNumber;
     }
 
-    public static long randomCard() {
-        Random random = new Random();
-        Long randomCode = random.nextLong();
-        return randomCode;
+    public static int[] randomPersonalCode() {
+
+
+        int[] arraysNumber = new int[10];
+
+        for (int i = 0; i < arraysNumber.length; i++) {
+
+            arraysNumber[i] = ApplicationContext.random.nextInt(10);
+        }
+        return arraysNumber;
     }
 }
